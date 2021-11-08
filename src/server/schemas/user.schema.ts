2cf1b985e320db546f296c50e9c98e837c8821f0
sirpuffin1 <angelcruz981@yahoo.com>
2021-11-08 14:58:08 -0800

    feat(add translation property to users)

--- src/server/schemas/user.schema.ts
+++ src/server/schemas/user.schema.ts
@@ -1,11 +1,18 @@
 import mongoose from 'mongoose';
 import type { User } from '../../shared/models/user.model';
+import type { Translation } from '../../shared/models/translation.model'
 const {Schema, model} = mongoose
+
+const translationSchema = new mongoose.Schema({
+    message: String
+})
 
 const userSchema = new Schema<User>({
     name: {type: String, required: true},
     email: {type: String, required: true},
     password: {type: String, required: true},
+    translations: [translationSchema]
+
 });
 
 export const UserModel = model<User>('User',userSchema)
---
+++ src/shared/models/translation.model.ts
@@ -0,0 +1,4 @@
+export interface Translation {
+    _id: string
+    message: string
+}
