f8919212289a3454b41cb4df09b345f37b1e978b
sirpuffin1 <angelcruz981@yahoo.com>
2021-12-01 10:49:11 -0800

    yess

--- dist/server/server.js
+++ dist/server/server.js
@@ -19,7 +19,7 @@ const io = new socketIO.Server(server, { cors: {
         origin: '*'
     } });
 const saltRounds = 10;
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 mongoose
     .connect(`${process.env.MONGO_URI}`)
     .then(() => {
--- src/server/server.ts
+++ src/server/server.ts
@@ -24,7 +24,7 @@ const io = new socketIO.Server(server,  { cors: {
 const saltRounds = 10;
 
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoose
   .connect(`${process.env.MONGO_URI}`)
